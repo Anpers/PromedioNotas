b565b138b82c504ee8330992d0aa3ed9706a4dde
Alex Huanca Perez <anpersax@gmail.com>
2026-07-20 21:49:00 -0400

    Se modifica el sistema para calcular cinco notas

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// =================================== 
-// Sistema de Promedio de Notas 
-// =================================== 
+// ===================================
+// Sistema de Promedio de Notas
+// ===================================
 const nota1 = 95;
-const nota2 = 85; 
-const nota3 = 90; 
-const promedio = (nota1 + nota2 + nota3) / 3;
+const nota2 = 85;
+const nota3 = 90;
+const nota4 = 80;
+const nota5 = 100;
 
-console.log("Sistema de Promedios"); 
-console.log("----------------------"); 
-console.log("Nota 1:", nota1); 
-console.log("Nota 2:", nota2); 
-console.log("Nota 3:", nota3); 
-console.log("Promedio:", promedio.toFixed(2)); 
+const promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
 
-console.log("----------------------"); 
-console.log("Fin del programa"); 
+console.log("Sistema de Promedios");
+console.log("--------------------");
+console.log("Nota 1:", nota1);
+console.log("Nota 2:", nota2);
+console.log("Nota 3:", nota3);
+console.log("Nota 4:", nota4);
+console.log("Nota 5:", nota5);
+console.log("Promedio:", promedio.toFixed(2));
+console.log("--------------------");
+console.log("Fin del programa");
